6292bc0dcd52b819e527a6430d0cbf3c7cb8c552
Pedro Ramon77 <ramonpedro775@gmail.com>
2025-04-01 16:30:12 -0300

    Adicionando exemplo de teste de Multiplicação  em JavaScript

--- teste_01.js
+++ teste_01.js
@@ -8,6 +8,6 @@ for (let i = 1; i <= 5; i++) {
 
 // Testando uma variável e operação
 let num1 = 10;
-let num2 = 5;
-let resultado = num1 - num2;
-console.log("A sub de " + num1 + " e " + num2 + " é: " + resultado);
+let num2 = 10;
+let resultado = num1 * num2;
+console.log("A multiplcação de " + num1 + " e " + num2 + " é: " + resultado);
